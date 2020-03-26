1312f4e1aee4260976d40ce93aa81b228229ee34
takayama <takayama@foxmail.com>
2020-03-26 11:39:37 +0900

    dhs

--- index.js
+++ index.js
@@ -8,30 +8,63 @@ process.on('unhandledRejection', (reason, promise)=>{
     fs.appendFileSync('err.log', Date() + ' Unhandled Rejection at:' + promise + 'reason:' + reason + '\n')
 });
 
-const WebSocket = require('ws')
+// const WebSocket = require('ws')
+
 const main = require('./main')
-const wss = new WebSocket.Server({ port: 3000 })
-wss.on('connection', (ws)=>{
-    ws.on('message', async(message)=>{
-        let data = JSON.parse(message)
-        const reply = (msg)=>{
-            let res = {
-                'action': '.handle_quick_operation',
-                'params': {
-                    'context': data,
-                    'operation': {
-                        'reply': typeof msg === 'string' ? msg : JSON.stringify(msg)
-                    }
+// const wss = new WebSocket.Server({ port: 3000 })
+// wss.on('connection', (ws)=>{
+//     ws.on('message', async(message)=>{
+//         let data = JSON.parse(message)
+//         const reply = (msg)=>{
+//             let res = {
+//                 'action': '.handle_quick_operation',
+//                 'params': {
+//                     'context': data,
+//                     'operation': {
+//                         'reply': typeof msg === 'string' ? msg : JSON.stringify(msg)
+//                     }
+//                 }
+//             }
+//             ws.send(res)
+//         }
+//         if (data.post_type === 'message' && data.message.substr(0, 3).toLowerCase() === 'dhs') {
+//             let parmas = data.message.substr(1).split(' ')
+//             let cmd = parmas[0].substr(3)
+//             let param = parmas[1]
+//             if (data.user_id === 372914165) { //调试代码
+//                 delete require.cache[require.resolve('./main.js')]
+//                 let main = require('./main.js')
+//                 let result = await main(data, cmd, param)
+//                 reply(result)
+//             }
+//         }
+//     })
+// })
+
+const http = require('http')
+http.createServer((req, res)=>{
+
+    let data = []
+    req.on('data', (d)=>data.push(d))
+    req.on('end', async()=>{
+        try {
+            data = Buffer.concat(gbl).toString()
+            data = JSON.parse(data)
+            if (data.post_type === 'message' && data.message.substr(0, 3).toLowerCase() === 'dhs') {
+                let parmas = data.message.substr(1).split(' ')
+                let cmd = parmas[0].substr(3)
+                let param = parmas[1]
+                if (data.user_id === 372914165) { //调试代码
+                    // delete require.cache[require.resolve('./main.js')]
+                    // let main = require('./main.js')
+                    let result = await main(data, cmd, param)
+                    res.end({'reply': result})
+                    return
                 }
             }
-            ws.send(res)
-        }
-        if (data.post_type === 'message' && data.message.substr(0, 3).toLowerCase() === 'dhs') {
-            let parmas = data.message.substr(1).split(' ')
-            let cmd = parmas[0].substr(3)
-            let param = parmas[1]
-            let result = await main(data, cmd, param)
-            reply(result)
+            res.end()
+        } catch (e) {
+            res.end()
         }
     })
-})
+}).listen(3001)
--- main.js
+++ main.js
@@ -13,8 +13,8 @@ if (fs.existsSync('./db')) {
 }
 setInterval(saveDb, 300000)
 process.on('exit', saveDb)
-const eid = 70424026
-dhs.start('13564803353@qq.com', '552233')
+const eid = 25331349 //70424026
+dhs.start('372914165@qq.com', '552233')
 
 const callApi = async(method, cid, param)=>{
     return new Promise((resolve, reject)=>{
@@ -29,7 +29,21 @@ const callApi = async(method, cid, param)=>{
     })
 }
 
-const help = ``
+const help = `-----dhs指令说明-----
+第①步 在大会室后台将 ${eid}(比赛小助手) 设置为比赛管理
+第②步 使用"dhs绑定 赛事id"指令将qq群和比赛绑定(赛事id是6位数字)
+第③步 就可以用下面的指令啦
+dhs情报 ※查看赛事基本信息和规则
+dhs名单 ※查看选手名单
+dhs公告 ※查看公告
+dhs大厅 ※查看大厅中的对局，和准备中的玩家
+★下面的命令群管理员才能使用
+dhs绑定 赛事id ※暂时一个群只能绑定一个比赛
+dhs解绑 ※解除绑定
+dhs添加 id1,id2,id3 ※添加选手
+dhs删除 id1,id2,id3 ※删除选手
+dhs重置 id1,id2,id3 ※只保留指定选手(参数为空会删除全部选手，慎用)
+dhs开赛 昵称1,昵称2,昵称3,昵称4 ※少设置选手会自动加电脑`
 
 const main = async(data, cmd, param)=>{
     let gid = data.group_id
@@ -43,15 +57,16 @@ const main = async(data, cmd, param)=>{
     if (hasCid)
         cid = db[gid].cid
     if (cmd === '')
-        return 'help'
+        return help
     else if (!hasCid && cmd !== '绑定')
         return '尚未绑定比赛'
     else {
         try {
+            let res
             switch (cmd) {
                 case '绑定':
                     if (hasCid)
-                        return '已经绑定过比赛了，需要先解绑。'
+                        return '已经绑定过比赛了，需要先解绑才能再次绑定。'
                     let cid = parseInt(param)
                     if (db.cids.has(cid))
                         return '该比赛已经被绑定了。'
@@ -65,22 +80,22 @@ const main = async(data, cmd, param)=>{
                         return '尚未绑定比赛。'
                     delete db[gid].cid
                     db.cids.delete(cid)
-                    return "解绑成功。(为了安全请务必同时删除大会室后台的管理权限)"
+                    return "解绑成功。(为了安全请务必删除大会室后台的管理权限)"
                     break
-                case '规则':
-                    let res = callApi('fetchContestGameRule', cid)
+                case '情报':
+                    res = callApi('fetchContestGameRule', cid)
                     return res
                     break
                 case '公告':
-                    let res = callApi('fetchContestNotice', cid)
+                    res = callApi('fetchContestNotice', cid)
                     return res
                     break
                 case '名单':
-                    let res = callApi('fetchContestPlayer', cid)
+                    res = callApi('fetchContestPlayer', cid)
                     return res
                     break
                 case '大厅':
-                    let res = callApi('startManageGame', cid)
+                    res = callApi('startManageGame', cid)
                     return res
                     break
                 case '添加':
