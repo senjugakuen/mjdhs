c0da70dc76627170673c8c2242e88064dfa81e72
takayama <takayama@foxmail.com>
2020-03-26 12:27:47 +0900

    dhs

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ http.createServer((req, res)=>{
                     // delete require.cache[require.resolve('./main.js')]
                     // let main = require('./main.js')
                     let result = await main(data, cmd, param)
-                    res.end(JSON.stringify({'reply': result}))
+                    res.end(JSON.stringify({'reply': typeof result === 'string' ? result : JSON.stringify(result)}))
                     return
                 }
             }
