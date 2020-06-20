198fadc329ae05272b252268c21cf09a0ad12812
takayama <takayama@foxmail.com>
2020-06-20 12:59:52 +0900

    1

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	port: 3001, //监听的端口
 	allowed: ["::ffff:172.17.0.2"], //允许连接的地址，空则无限制
-	dhs_url: 'wss://gateway-v2.maj-soul.com:9553', //雀魂后台地址，一般不用修改
+	dhs_url: 'wss://contest-vex.maj-soul.com', //雀魂后台地址，一般不用修改
 	eid: '25331349(查询)', //雀魂加好友ID
 	master: [372914165,2308941253] //主人QQ列表
 }
