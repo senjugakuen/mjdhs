0b79a77a66b1ab52053b51b4177dccb95decf70f
takayama <takayama@foxmail.com>
2020-07-17 13:37:41 +0900

    1

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
 	dhs_url: 'wss://contest-vex.maj-soul.com', //雀魂后台地址，一般不用修改
 	eid: '25331349(查询)', //雀魂加好友ID
 	master: [372914165,2308941253], //主人QQ列表
-    cqhttp_url: "http://usk.lietxia.bid:5700"
+    cqhttp_url: "http://172.17.0.2:5700" //cqhttp API地址
 }
