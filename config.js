c43e069f2950438f61a4c6bf07bd8c06f16de4e6
takayama <takayama@foxmail.com>
2020-08-16 00:35:54 +0900

    config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
 	port: 3001, //监听的端口
-	allowed: ["::ffff:172.17.0.2"], //允许连接的地址，空则无限制
+	allowed: [], //允许连接的地址，空则无限制
 	dhs_url: 'wss://contest-vex.maj-soul.com', //雀魂后台地址，一般不用修改
 	eid: '25331349(查询)', //雀魂加好友ID
 	master: [372914165,2308941253], //主人QQ列表
-    cqhttp_url: "http://172.17.0.2:5700" //cqhttp API地址
+    cqhttp_url: "http://localhost:5700" //cqhttp API地址
 }
