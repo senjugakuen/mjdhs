4bdfb8272c4969805e2f8906ed2a2b8997b8ef99
takayama <takayama@foxmail.com>
2022-06-11 21:02:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
-    dhs_url: 'wss://gateway-v2.maj-soul.com:9553', //雀魂后台地址，一般不用修改
+    dhs_url: 'wss://gateway-v2.maj-soul.net/contest_ws_gateway', //雀魂后台地址，一般不用修改
     eid: '25331349(查询)', //雀魂加好友ID
     master: [372914165,2308941253], //主人QQ列表
     account: "372914165@qq.com", //雀魂登录账号
